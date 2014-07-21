dc5c01dc8655780dc21bbeaf42f867459fdaf331
mindaugas <mindaugas.grigas@visma.com>
2014-07-21 15:04:49 +0300

    initial commit

---
+++ MyFighter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodeFights.model;
+
+namespace CodeFights
+{
+    class MyFighter : IFighter
+    {
+        public Move MakeNextMove(Move opponentsLastMove, int myLastScore, int opponentsLastScore)
+        {
+            return new Move().AddAttack(Area.Nose).SetComment("your impl goes here");
+        }
+    }
+}
---
+++ model/GameScoringRules.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeFights.model
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class GameScoringRules
+    {
+
+        public static int LIFEPOINTS = 150;
+
+        public static int CalculateScore(IList<Area> attackAreas, IList<Area> blockAreas)
+        {
+            int rez = 0;
+
+            if (attackAreas == null)
+                return rez;
+
+            foreach (Area attack in attackAreas)
+                if (blockAreas.Contains(attack) == false)
+                    rez += (int)attack;
+
+            return rez;
+        }
+
+    }
+}
